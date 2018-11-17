1c705384af78d5a27157e4450d65c4c767b2a4dc
Ricardo La Rosa <ricardo@larosasanoja.com>
2018-11-17 00:07:59 +0000

    Implementing resource kaleido_invitation

---
+++ kaleido/resource_invitation.go
@@ -0,0 +1,110 @@
+// Copyright 2018 Kaleido, a ConsenSys business
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package kaleido
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	kaleido "github.com/kaleido-io/kaleido-sdk-go/kaleido"
+)
+
+func resourceInvitation() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceInvitationCreate,
+		Read:   resourceInvitationRead,
+		Delete: resourceInvitationDelete,
+		Schema: map[string]*schema.Schema{
+			"consortium_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"org_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"email": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceInvitationCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(kaleido.KaleidoClient)
+	invitation := kaleido.NewInvitation(d.Get("org_name").(string), d.Get("email").(string))
+	consortiumId := d.Get("consortium_id").(string)
+
+	res, err := client.CreateInvitation(consortiumId, &invitation)
+
+	if err != nil {
+		return err
+	}
+
+	status := res.StatusCode()
+	if status != 201 {
+		msg := "Failed to create invitation %s in consortium %s with status %d"
+		return fmt.Errorf(msg, invitation.OrgName, consortiumId, status)
+	}
+
+	d.SetId(invitation.Id)
+	return nil
+}
+
+func resourceInvitationRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(kaleido.KaleidoClient)
+	consortiumId := d.Get("consortium_id").(string)
+
+	var invitation kaleido.Invitation
+	res, err := client.GetInvitation(consortiumId, d.Id(), &invitation)
+
+	if err != nil {
+		return err
+	}
+
+	status := res.StatusCode()
+	if status != 200 {
+		msg := "Failed to find invitation %s in consortium %s with status %d"
+		return fmt.Errorf(msg, invitation.OrgName, consortiumId, status)
+	}
+
+	d.Set("org_name", invitation.OrgName)
+	d.Set("email", invitation.Email)
+	return nil
+}
+
+func resourceInvitationDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(kaleido.KaleidoClient)
+	consortiumId := d.Get("consortium_id").(string)
+	invitationId := d.Id()
+
+	res, err := client.DeleteInvitation(consortiumId, invitationId)
+
+	if err != nil {
+		return err
+	}
+
+	status := res.StatusCode()
+	if status != 204 {
+		msg := "Failed to delete invitation %s in consortium %s with status: %d"
+		return fmt.Errorf(msg, invitationId, consortiumId, status)
+	}
+
+	d.SetId("")
+	return nil
+}
