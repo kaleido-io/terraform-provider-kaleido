4af8d7fe7a125e227dd5d07a48c4df04f0724773
Trevor Scanlon <trevor.scanlon@kaleido.io>
2024-04-18 16:31:21 -0400

    tested addition of bir with .terraformrc file for local build of the provider

--- examples/platform_stack/main.tf
+++ examples/platform_stack/main.tf
@@ -187,6 +187,31 @@ resource "kaleido_platform_service" "cms_0" {
   config_json = jsonencode({})
 }
 
+resource "kaleido_platform_runtime" "bir_0"{
+  type = "BlockIndexer"
+  name = "block_indexer"
+  environment = kaleido_platform_environment.env_0.id
+  config_json = jsonencode({})
+}
+
+resource "kaleido_platform_service" "bis_0"{
+  type = "BlockIndexer"
+  name = "block_indexer"
+  environment = kaleido_platform_environment.env_0.id
+  runtime = kaleido_platform_runtime.bir_0.id
+  config_json=jsonencode(
+    {
+      contractManager = {
+        id = kaleido_platform_service.cms_0.id
+      }
+      evmGateway = {
+        id = kaleido_platform_service.gws_0.id
+      }
+    }
+  )
+  hostnames = {"${kaleido_platform_network.net_0.name}" = ["ui", "rest"]}
+}
+
 resource "kaleido_platform_runtime" "amr_0" {
   type = "AssetManager"
   name = "asset_manager1"
