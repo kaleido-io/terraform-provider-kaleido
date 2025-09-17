d4547fedbba355df81ecc5a3f85aa4f47eadf9ae
hfuss <hayden.fuss@kaleido.io>
2025-09-16 21:12:28 -0400

    admin example
    Signed-off-by: hfuss <hayden.fuss@kaleido.io>

--- examples/resources/kaleido_platform_user/resource.tf
+++ examples/resources/kaleido_platform_user/resource.tf
@@ -4,3 +4,13 @@ resource "kaleido_platform_user" "user" {
   email = "user@example.com"
   sub   = "user-subject-identifier"
 }
+
+// A simple example of a platform user with admin privileges
+resource "kaleido_platform_user" "admin_user" {
+  name  = "preferred-username"
+  email = "user@example.com"
+  sub   = "user-subject-identifier"
+  is_admin = true
+}
+
+// See 'kaleido_platform_group_membership' and 'kaleido_platform_group' for more examples of how to grant user's least privilege access to resources
